603ba1029225fe61e5ff167783f8cdb7281c7301
Michael Friend <mfriend@lutron.com>
2019-10-25 14:45:32 -0400

    AI-3.5 <Lutron@PC12649.local Create baseRefactoring.xml, vim_settings.xml, ide.general.xml, Class header.java, project.default.xml, rainbow_brackets.xml, diff.xml, Singleton.java, Enum.java, Interface.java, git.xml, Default.xml, androidStudioFirstRun.xml, find.xml, ui.lnf.xml, File Header.java, androidEditors.xml, plugin_ui.xml, AndroidLog.xml, colors.scheme.xml, KeyPromoterXSettings.xml, notifications.xml, _@user_Darcula.icls, path.macros.xml, AnnotationType.java, consoleFolding.xml, Default.xml, studioFlags.xml, parameter.hints.xml, Class.java, jdk.table.xml, laf.xml, Mac OS X 10_5_ copy.xml, abbreviations.xml, customPostfixTemplates.xml, vcs.xml, editor.codeinsight.xml, Android.xml, toStringTemplates.xml, keymap.xml, editor.xml, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/Class header.java
@@ -0,0 +1,5 @@
+/** 
+ * TODO: Write Class header
+ * Created by MFriend on ${DATE}.
+ * Copyright (c) ${YEAR} Lutron. All rights reserved.
+ */
---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,7 @@
+#set($USER = "MFriend")
+/** 
+ * TODO MRF Write Class header 
+ *
+ * Created by ${USER} on ${DATE}.
+ * Copyright (c) ${YEAR} Lutron. All rights reserved.
+ */
